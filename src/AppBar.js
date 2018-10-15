32788f941920bb8b9fc4737b57da618faaa3d3ca
daeunnpark <daeunn@gmail.com>
2018-10-15 16:51:31 -0400

    UI updated with theme color/name/categories

--- src/AppBar.js
+++ src/AppBar.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { AppBar, UserMenu, MenuItemLink, translate } from 'react-admin';
-import SettingsIcon from '@material-ui/icons/Settings';
+import React from "react";
+import { AppBar, UserMenu, MenuItemLink, translate } from "react-admin";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 const CustomUserMenu = translate(({ translate, ...props }) => (
-    <UserMenu {...props}>
-        <MenuItemLink
-            to="/configuration"
-            primaryText={translate('pos.configuration')}
-            leftIcon={<SettingsIcon />}
-        />
-    </UserMenu>
+  <UserMenu {...props}>
+    <MenuItemLink
+      to="/configuration"
+      primaryText={translate("pos.configuration")}
+      leftIcon={<SettingsIcon />}
+    />
+  </UserMenu>
 ));
 
+/*
+
+  
+   <IconMenu color={white}
+        iconButtonElement={
+            <IconButton><ViewModule color={white} /></IconButton>
+        }
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+    >
+        <MenuItem key={1} primaryText="Application 1" />
+        <MenuItem key={2} primaryText="Application 2" />
+        <MenuItem key={3} primaryText="Application 3" />
+    </IconMenu>
+
+*/
+
 const CustomAppBar = props => (
-    <AppBar {...props} userMenu={<CustomUserMenu />} />
+  <AppBar {...props} userMenu={<CustomUserMenu />} />
 );
 
 export default CustomAppBar;
